cd51885d2d3ebdaa6162f43916a18b7e015664e0
Manjinder <smanjinder96.ms@gmail.com>
2021-10-08 12:27:54 -0700

    Alternate Solution using count method

--- hackerRank/sparseArray.py
+++ hackerRank/sparseArray.py
@@ -3,14 +3,20 @@ strings = ['ab', 'ab', 'a', 'a', 'abc']
 
 
 def sparseArray(queries, strings):
-    newArr = []
-    for i in range(len(queries)):
-        newArr.append(0)
-    for string in strings:
-        for index, query in enumerate(queries, 0):
-            if(query == string):
-                newArr[index] = newArr[index]+1
-    return newArr
+    # newArr = []
+    # for i in range(len(queries)):
+    #     newArr.append(0)
+    # for string in strings:
+    #     for index, query in enumerate(queries, 0):
+    #         if(query == string):
+    #             newArr[index] = newArr[index]+1
+    # return newArr
+
+    #Alternate solution
+    results = []
+    for item in queries:
+        results.append(strings.count(item))
+    return results
 
 
 print(sparseArray(queries, strings))
